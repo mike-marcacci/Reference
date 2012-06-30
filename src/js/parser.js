b40628a8cfc294ec311d0a9ea49db2903359d469
Michael Marcacci <mike.marcacci@gmail.com>
2012-06-29 18:51:27 -0700

    Add bindings in parse function

--- src/js/parser.js
+++ src/js/parser.js
@@ -12,27 +12,27 @@
           highlight.appendChild(wordClone);
           wordNode.parentNode.replaceChild(highlight, wordNode);
           
-		  highlight = $(highlight);
+          highlight = $(highlight);
 		  
-          highlight.addClass('reference-link ' + triggerData.theme)
-		  var tagData = {}; tagData[trigger] = triggerData;
-		  highlight.data('referenceTriggers', tagData)
+          highlight.addClass('reference-link ' + triggerData.theme).addClass('on-' + trigger)
+          var tagData = {}; tagData[trigger] = triggerData;
+          highlight.data('referenceTriggers', tagData)
 		  
           return 1; //skip added node in parent
         }
       } else if ($(node).hasClass('reference-link') && $(node).data('referenceTriggers')) { // already highlighted text nodes 
 		  
-		  var highlight = $(node);
-		  if(highlight.data('referenceTriggers')[trigger]) {
-			  $.unique($.merge(highlight.data('referenceTriggers')[trigger]['collections'], triggerData['collections']))
-			  // add to collections
-			  highlight.data('referenceTriggers')[trigger]['theme'] = triggerData['theme'];
-			  // replace theme
-		  } else {
-		  	highlight.data('referenceTriggers')[trigger] = triggerData;
-		  }
+        var highlight = $(node);
+        if(highlight.data('referenceTriggers')[trigger]) {
+          $.unique($.merge(highlight.data('referenceTriggers')[trigger]['collections'], triggerData['collections']))
+          // add to collections
+          highlight.data('referenceTriggers')[trigger]['theme'] = triggerData['theme'];
+          // replace theme
+        } else {
+          highlight.data('referenceTriggers')[trigger] = triggerData;
+        }
 		  
-		  //$.unique($.merge($(node).data('referenceCollections'),tagData.referenceCollections));
+        //$.unique($.merge($(node).data('referenceCollections'),tagData.referenceCollections));
       } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
       !/(script|style)/i.test(node.tagName)) { // ignore script and style nodes
         for (var i = 0; i < node.childNodes.length; i++) {
@@ -47,7 +47,7 @@
   
   $.fn.addReferences = function(o){
     
-    var linkEl = $(this);
+    var searchEl = $(this);
     
     var options = {
       collections: [],
@@ -79,7 +79,7 @@
 
       var pattern = "(" + terms.join("|") + ")";
       if (options.exactMatch) {
-          pattern = "\\b" + pattern + "\\b";
+        pattern = "\\b" + pattern + "\\b";
       }
       
       var re = new RegExp(pattern, collection.caseSensitive ? "" : "i");
@@ -88,17 +88,43 @@
         theme: options.theme,
         collections: [collection.id]
       }
+
+      
+      /*******************************************
+      Add the reference links
+      ********************************************/
+      
     
-      linkEl.each(function () {
-          jQuery.addReferences(this, re, options.trigger, triggerData);
+      searchEl.each(function () {
+        jQuery.addReferences(this, re, options.trigger, triggerData);
       });
-      
-      
-      
-      // bind options.trigger to linkEl
-	  	
+	
     })
     
+
+      
+      
+
+      
+    /*******************************************
+    Bind reference to links
+    ********************************************/
+    
+	  
+    var triggerBound = false;
+    if(searchEl.data('events') && searchEl.data('events')[options.trigger]){
+      $.each(searchEl.data('events')[options.trigger], function(i){
+        if(this.selector == ".reference-link.on-" + options.trigger) {
+          triggerBound = true;
+        }
+      });
+    }
+    
+    if(!triggerBound) {
+      searchEl.on(options.trigger, ".reference-link.on-" + options.trigger, $.fn.reference)
+    }
+    
+	  
     
     
     return $(this);
--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,20 +1,19 @@
 (function( $ ) {
   
   $.extend({
-    reference: function (linkEl, o) {
-      var options = $.extend($(linkEl).data(), o);
+    reference: function (linkEl, event) {
 	  
 	  // find matching collections from data-referenceCollections
 	  
 	  // display the reference panel
 	  
-	  
+	  console.log($(linkEl).data('referenceTriggers')[event.type])
 	  
     }
   });
   
-  $.fn.reference = function(o){
-    $.reference(this, o);
+  $.fn.reference = function(event){
+    $.reference(this, event);
     return $(this);
   }
   
