954019f576ab3193d3d2d4bcc3ac858b11b543c9
Michael Marcacci <mike.marcacci@gmail.com>
2012-07-05 13:22:09 -0700

    Make option for Regex keys; Closes issue #3

--- src/js/parser.js
+++ src/js/parser.js
@@ -87,7 +87,10 @@
       
       // escape special characters in search terms
       terms = jQuery.map(terms, function(word, i) {
-
+        if(word.charAt(0) == '/' && word.charAt(word.length-1) == '/'){
+          return word.substr(1,word.length-2)
+        }
+        return word.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
       });
 
       var pattern = "(" + terms.join("|") + ")";
